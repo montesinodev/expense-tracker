8342f7e8b0667d17638036e75f84e183d5ea8419
Marlon Montesino <montesinodev@gmail.com>
2025-09-04 01:25:12 -0600

    Made changes and fixed bugs

--- vite.config.gh-pages.js
+++ vite.config.gh-pages.js
@@ -1,8 +1,8 @@
-// vite.config.js (for Vercel)
+// vite.config.gh-pages.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  // NO base property here - Vercel needs root paths
+  base: '/expense-tracker/' // GitHub Pages needs this
 })
